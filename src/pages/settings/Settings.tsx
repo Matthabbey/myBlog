422314fe262e119575f3a01a6f2977bbc3ee9577
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-01-04 00:47:14 +0100

    settings component

---
+++ src/pages/settings/Settings.tsx
@@ -0,0 +1,36 @@
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./setting.css";
+import { FaRegUserCircle } from 'react-icons/fa'
+
+const Settings = () => {
+  return (
+    <div className="settings">
+      <div className="settingWrapper">
+        <div className="settingTitle">
+          <span className="settingsUpdateTitle">Update Your Account</span>
+          <span className="settingsDeleteTitle">Delete Account</span>
+        </div>
+        <form className="settingsForm">
+          <label htmlFor="">Profile Picture</label>
+          <div className="settingsPP">
+            <img src={"https://source.unsplash.com/random/600x300?sig=1"} alt="" />
+            <label htmlFor="fileInput">
+              <FaRegUserCircle className="settingsPPIcon"/>
+            </label>
+            <input type="file" id="fileInput" style={{display: "none"}}/>
+          </div>
+          <label>Username</label>
+          <input type="text" placeholder="ayoola" />
+          <label>Email</label>
+          <input type="email" placeholder="Email" />
+          <label>Password</label>
+          <input type="password" placeholder="Password" />
+          <button className="settingsSubmit">Update</button>
+        </form>
+      </div>
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Settings;
--- src/pages/write/Write.tsx
+++ src/pages/write/Write.tsx
@@ -4,12 +4,13 @@ import {GrAdd} from 'react-icons/gr'
 const Write = () => {
   return (
     <div className='write'>
+        <img className='writeImg' src={"https://source.unsplash.com/random/1400x750?sig=1"} alt="" />
         <form className="writeForm">
             <div className="writeFormGroup">
                 <label htmlFor="fileInput">
-                    <GrAdd />
+                    <GrAdd className='writeIcon'/>
                 </label>
-                <input type="file" className='fileInput'/>
+                <input type="file" id='fileInput' style={{display: "none"}}/>
                 <input type="text"  placeholder='Title' className='writeInput' autoFocus={true}/>
             </div>
             <div className="writeFormGroup">
