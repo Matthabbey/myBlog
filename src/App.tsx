0f17e61d00062d5971cb850cb8cfca0eb025ece3
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-01-03 20:39:13 +0100

    singlePost component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react"
 import Home from "./pages/home/Home"
-import Topbar from "./topbar/Topbar"
+import Topbar from "./components/topbar/Topbar"
+import Single from "./pages/single/Single"
 
 function App() {
 
   return (
     <div>
       <Topbar />
-      <Home/>
+      <Single/>
     </div>
   )
 }
---
+++ src/components/singlePost/SinglePost.tsx
@@ -0,0 +1,50 @@
+import "./singlePost.css";
+import { FiEdit } from "react-icons/fi";
+import { RiDeleteBinLine } from "react-icons/ri";
+
+const SinglePost = () => {
+  return (
+    <div className="singlePost">
+      <div className="singlePostWrapper">
+        <img
+          className="singlePostImg"
+          src={"https://source.unsplash.com/random/900x550?sig=1"}
+          alt=""
+        />
+        <h1 className="singlePostTitle">
+          Lorem ipsum dolor sit amet elit.
+          <div className="singlePostEdit">
+            <FiEdit className="singlePostIcon" />
+            <RiDeleteBinLine className="singlePostIcon" />
+          </div>
+        </h1>
+        <div className="singlePostInfo">
+          <span className="singlePostAuthor">
+            Author: <b>Ayoola</b>
+          </span>
+          <span className="singlePostDate"> 1 hour ago</span>
+        </div>
+        <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non delectus
+          iure iste similique, quam labore quidem natus corporis vel adipisci
+          exercitationem, ut minus molestias earum, accusamus quasi architecto
+          tempora soluta.Lorem ipsum dolor sit amet consectetur adipisicing elit. Non delectus
+          iure iste similique, quam labore quidem natus corporis vel adipisci
+          exercitationem, ut minus molestias earum, accusamus quasi architecto
+          tempora soluta.Lorem ipsum dolor sit amet consectetur adipisicing elit. Non delectus
+          iure iste similique, quam labore quidem natus corporis vel adipisci
+          exercitationem, ut minus molestias earum, accusamus quasi architecto
+          tempora soluta.Lorem ipsum dolor sit amet consectetur adipisicing elit. Non delectus
+          iure iste similique, quam labore quidem natus corporis vel adipisci
+          exercitationem, ut minus molestias earum, accusamus quasi architecto
+          tempora soluta.Lorem ipsum dolor sit amet consectetur adipisicing elit. Non delectus
+          iure iste similique, quam labore quidem natus corporis vel adipisci
+          exercitationem, ut minus molestias earum, accusamus quasi architecto
+          tempora soluta.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SinglePost;
